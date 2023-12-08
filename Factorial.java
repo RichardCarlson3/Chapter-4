1f1a3dd30c1086974aa0fdcd8f469734e8630c11
richardcarlson3 <richardcarlson@billericak12.com>
2023-12-08 10:22:58 -0500

    hi

--- Factorial.java
+++ Factorial.java
@@ -6,10 +6,37 @@ public class Factorial {
         }
         return sum;
     }
+    public double calcE(){
+        double e=1;
+        double olde=0;
+        int a=1;
+        while(e-olde > .001){
+            olde=e;
+            e+=((double)1/calcFactorial(a));
+            a++;
+        }
+        return e;
+
+        }
+    public double calcEX(int x){
+        double e=1;
+        int y=1;
+        double olde=0;
+        int a=1;
+        while(e-olde > .001){
+            olde=e;
+            e+=((double)Math.pow(x,y)/calcFactorial(a));
+            a++;
+            y++;
+        }
+        return e;
+    }
     public static void main(String [] args){
         Factorial a= new Factorial();
         for(int b=1; b<=20; b++){
         System.out.println(a.calcFactorial(b));
         }
+        System.out.printf("e is %2.3f \n", a.calcE());
+        System.out.printf("e is %2.3f \n", a.calcEX(2));
     }
 }
