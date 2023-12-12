e2a0e231cc1233f4ad13a6cbb23d9f05595d1a27
richardcarlson3 <richardcarlson@billericak12.com>
2023-12-12 10:23:17 -0500

    hi

---
+++ NestedForLoops.java
@@ -0,0 +1,9 @@
+public class NestedForLoops {
+    public int addEvens(int n){
+        for(int a=2; )
+        return sum
+    }
+    public static void main(String [] args){
+        NestedForLoops a= new NestedForLoops();
+    }
+}
